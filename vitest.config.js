b736ea571a24229327350e654e474b44cf8d244e
deMGoncalves <cleber.engineer@hotmail.com>
2025-05-15 15:44:10 -0300

    ci(vitest): configuracao dos testes

---
+++ vitest.config.js
@@ -0,0 +1,24 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	resolve: {
+		alias: {
+			"@directive": resolve(__dirname, "packages/directive"),
+		},
+	},
+	test: {
+		coverage: {
+			include: ["packages/**/*.{js,ts}"],
+			exclude: ["packages/**/index.{js,ts}"],
+			reporter: ["text"],
+			thresholds: {
+				statements: 80,
+				branches: 80,
+				functions: 80,
+				lines: 80,
+			},
+		},
+		environment: "jsdom",
+	},
+});
