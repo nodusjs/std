b93c96e6a228129d5758628a9593e558980bdeb4
deMGoncalves <cleber.engineer@hotmail.com>
2025-05-17 00:13:27 -0300

    feat(disconnected): modulo para connectar excucao de um metodo quando o evento disconnected callback for executado

---
+++ packages/directive/disconnected/disconnected.js
@@ -0,0 +1,37 @@
+/**
+ * Decorator que executa um método ao remover o Custom Element do DOM.
+ *
+ * @param {Object} target - Protótipo do Custom Element decorado.
+ * @param {string|Symbol} method - Nome do método a ser executado.
+ * @returns {void}
+ *
+ * @description
+ * O decorator `disconnected` registra um método específico para ser executado
+ * automaticamente sempre que o Custom Element for desconectado do DOM,
+ * utilizando a API `disconnectedCallback` com Proxy para manter o fluxo original.
+ *
+ * @example
+ * import { disconnected } from '@nodusjs/std/directive';
+ *
+ * class MyElement extends HTMLElement {
+ *   @disconnected
+ *   cleanup() {
+ *     console.log('Elemento removido do DOM.');
+ *   }
+ * }
+ *
+ * customElements.define('my-element', MyElement);
+ */
+const disconnected = (target, method) => {
+	target.disconnectedCallback = new Proxy(
+		target.disconnectedCallback ?? (() => {}),
+		{
+			apply(original, context, args) {
+				original.apply(context, args);
+				context[method]();
+			},
+		},
+	);
+};
+
+export default disconnected;
--- packages/directive/index.js
+++ packages/directive/index.js
@@ -1,2 +1,3 @@
 export { default as connected } from "./connected";
+export { default as disconnected } from "./disconnected";
 export { default as define } from "./define";
