29edaf0db2267ff62a91c126c112106c4504b41e
deMGoncalves <cleber.engineer@hotmail.com>
2025-05-18 19:30:16 -0300

    feat(dom): contrucao do paint

---
+++ packages/dom/index.js
@@ -0,0 +1,1 @@
+export { default as paint } from "./paint";
---
+++ packages/dom/paint/paint.js
@@ -0,0 +1,34 @@
+import render from "@dom/render";
+
+/**
+ * Decorator que renderiza um componente com estilos ao ser conectado ao DOM.
+ *
+ * @param {Function} component - Função responsável por retornar o HTML do componente. Recebe `this` como argumento.
+ * @param {...Function} styles - Funções que retornam `CSSStyleSheet`. Cada uma recebe `this` como argumento.
+ * @returns {void}
+ *
+ * @description
+ * O decorator `paint` permite executar a renderização de um componente customizado
+ * no momento em que ele é conectado ao DOM, aplicando também estilos encapsulados
+ * via `adoptedStyleSheets` no `shadowRoot` ou diretamente no próprio elemento.
+ *
+ * Ele suporta a execução dos callbacks `willPaintCallback`, `paintCallback` e `didPaintCallback`
+ * para permitir controle sobre o ciclo de renderização.
+ *
+ * @example
+ * import { paint } from '@nodusjs/std/dom';
+ *
+ * const render = (el) => `<p>Hello, ${el.name}</p>`;
+ * const style = (el) => new CSSStyleSheet();
+ *
+ * @paint(render, style)
+ * class MyElement extends HTMLElement {
+ *   name = 'world';
+ * }
+ */
+const paint =
+	(component, ...styles) =>
+	(target) =>
+		render(component).with(styles).from(target).whenConnected();
+
+export default paint;
