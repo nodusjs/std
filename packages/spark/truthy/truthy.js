369dc30a0fda5b7c0408adb957274bd8f5b6bbb5
deMGoncalves <cleber.engineer@hotmail.com>
2025-05-22 12:02:48 -0300

    fix(spark): erro de export

--- packages/spark/truthy/truthy.js
+++ packages/spark/truthy/truthy.js
@@ -17,12 +17,10 @@
  * truthy("true");   // true
  * truthy("");       // true
  */
-function truthy(value) {
+export function truthy(value) {
 	if (value === "no") return false;
 	if (value === "false") return false;
 	if (value === "0") return false;
 	if (value === null) return false;
 	return true;
 }
-
-export default truthy;
