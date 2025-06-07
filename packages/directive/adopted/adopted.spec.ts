83a292e21f5d78d105cb25e4c061a3958918f4a8
deMGoncalves <cleber.engineer@hotmail.com>
2025-06-07 11:49:13 -0300

    test(adoptes): organizacao do codigo

--- packages/directive/adopted/adopted.spec.ts
+++ packages/directive/adopted/adopted.spec.ts
@@ -1,4 +1,6 @@
+import execute from "@directive/execute";
 import { describe, expect, it, vi } from "vitest";
+import adopted from "./adopted";
 
 vi.mock("@directive/execute", () => {
   return {
@@ -8,24 +10,21 @@ vi.mock("@directive/execute", () => {
   };
 });
 
-import execute from "@directive/execute";
-import adopted from "./adopted";
-
 describe("adopted", () => {
   it("deve encadear corretamente execute -> on -> after", () => {
-    const after = vi.fn();
-    const on = vi.fn(() => ({ after }));
-    const exec = vi.fn(() => ({ on }));
+    const afterMock = vi.fn();
+    const onMock = vi.fn(() => ({ after: afterMock }));
+    const executeMock = vi.fn(() => ({ on: onMock }));
 
-    execute.mockImplementation(exec);
+    execute.mockImplementation(executeMock);
 
     class MyElement {
       @adopted
       onAdopted() {}
     }
 
-    expect(exec).toHaveBeenCalledWith("onAdopted");
-    expect(on).toHaveBeenCalledWith(MyElement.prototype);
-    expect(after).toHaveBeenCalledWith("adoptedCallback");
+    expect(executeMock).toHaveBeenCalledWith("onAdopted");
+    expect(onMock).toHaveBeenCalledWith(MyElement.prototype);
+    expect(afterMock).toHaveBeenCalledWith("adoptedCallback");
   });
 });
