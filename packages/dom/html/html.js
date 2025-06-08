0df00a00034379df9f98c86b2fecad9abfd820cc
deMGoncalves <cleber.engineer@hotmail.com>
2025-06-08 08:36:34 -0300

    docs(html): padronizando o jsdoc

--- packages/dom/html/html.js
+++ packages/dom/html/html.js
@@ -1,6 +1,11 @@
 /**
  * Template tag `html` para gerar strings HTML de forma segura e controlada.
  *
+ * @param {TemplateStringsArray} strings - Array com os pedaços literais do template.
+ * @param {...any} values - Valores interpolados no template.
+ * @returns {string} HTML combinado como string.
+ *
+ * @description
  * Essa função será utilizada no helper `component` para definir o conteúdo HTML
  * dos Web Components de forma declarativa, usando template literals.
  *
@@ -8,14 +13,10 @@
  * respeitando a interpolação dos dados sem aplicar nenhuma transformação
  * ou sanitização. Versões futuras podem incluir minificação, limpeza ou validação.
  *
- * @param {TemplateStringsArray} strings - Array com os pedaços literais do template.
- * @param {...any} values - Valores interpolados no template.
- * @returns {string} HTML combinado como string.
- *
  * @example
  * const name = "Clebão";
- * const htmlOutput = html`<h1>Olá, ${name}!</h1>`;
- * console.log(htmlOutput); // "<h1>Olá, Clebão!</h1>"
+ * const text = html`<h1>Olá, ${name}!</h1>`;
+ * console.log(text); // "<h1>Olá, Clebão!</h1>"
  */
 const html = (strings, ...values) => {
   return String.raw(
