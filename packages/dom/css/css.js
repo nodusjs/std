c09e4c31f8e72383cfd2e1b287bf15e1985db6c8
deMGoncalves <cleber.engineer@hotmail.com>
2025-06-08 08:16:30 -0300

    style(css): estruturacao do codigo

--- packages/dom/css/css.js
+++ packages/dom/css/css.js
@@ -1,12 +1,13 @@
 /**
  * Template tag `css` que gera uma instância de `CSSStyleSheet` a partir de um template literal.
  *
- * Ideal para uso em Web Components com `adoptedStyleSheets`, fornecendo uma API declarativa
- * para definir estilos encapsulados.
- *
  * @param {TemplateStringsArray} strings - Fragmentos de strings literais do template CSS.
  * @param {...any} values - Valores interpolados no template.
  * @returns {CSSStyleSheet} Uma instância de folha de estilo pronta para ser aplicada.
+ *
+ * @description
+ * Ideal para uso em Web Components com `adoptedStyleSheets`, fornecendo uma API declarativa
+ * para definir estilos encapsulados.
  *
  * @example
  * const primary = "blue";
@@ -21,8 +22,8 @@
  */
 const css = (strings, ...values) => {
   const styleSheet = new CSSStyleSheet();
-  const cssText = String.raw({ raw: strings }, ...values);
-  styleSheet.replaceSync(cssText);
+  const text = String.raw({ raw: strings }, ...values);
+  styleSheet.replaceSync(text);
   return styleSheet;
 };
 
